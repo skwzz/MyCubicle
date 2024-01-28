09c95e714867a82966422aa0e581ffeb8520292a
skwzz <kidol9108@gmail.com>
2024-01-28 14:43:50 +0900

    API에 대한 공통 응답객체 사용 정리

--- src/main/java/com/skwzz/sample/controller/SampleController.java
+++ src/main/java/com/skwzz/sample/controller/SampleController.java
@@ -1,15 +1,49 @@
 package com.skwzz.sample.controller;
 
+import com.skwzz.sample.dto.SampleDto;
+import com.skwzz.sample.service.SampleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api")
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/sample")
 @RestController
 public class SampleController {
 
-    @GetMapping
-    public String getSample(){
-        return "sample";
+    private final SampleService sampleService;
+
+    @GetMapping("/single")
+    public SampleDto getSample(){
+        return SampleDto.builder()
+                .col1("컬럼 1")
+                .col2("컬럼 2")
+                .build();
+    }
+
+    @GetMapping("/list")
+    public List<SampleDto> getSampleList(){
+        List<SampleDto> sampleDtoList = new ArrayList<>();
+        SampleDto sampleDto = SampleDto.builder()
+                .col1("컬럼 1")
+                .col2("컬럼 2")
+                .build();
+        for(int i=0; i<10; i++){
+            sampleDtoList.add(sampleDto);
+        }
+        return sampleDtoList;
+    }
+
+    @GetMapping("/error")
+    public SampleDto getError() throws Exception {
+        SampleDto dto = sampleService.getError();
+        return SampleDto.builder()
+                .col1("컬럼 1")
+                .col2("컬럼 2")
+                .build();
     }
 }
---
+++ src/main/java/com/skwzz/sample/dto/SampleDto.java
@@ -0,0 +1,11 @@
+package com.skwzz.sample.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SampleDto {
+    private String col1;
+    private String col2;
+}
