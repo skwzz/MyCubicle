d03b59c0c2c4da7a9df49ce63616746d44edc68d
skwzz <kidol9108@gmail.com>
2024-02-03 23:34:27 +0900

    서비스코드 추가

--- src/main/java/com/skwzz/sample/controller/SampleController.java
+++ src/main/java/com/skwzz/sample/controller/SampleController.java
@@ -1,15 +1,20 @@
 package com.skwzz.sample.controller;
 
 import com.skwzz.sample.dto.SampleDto;
+import com.skwzz.sample.service.SampleService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/sample")
 public class SampleController {
+
+    private final SampleService service;
 
     @GetMapping
     public String getSample(){
---
+++ src/main/java/com/skwzz/sample/service/SampleService.java
@@ -0,0 +1,9 @@
+package com.skwzz.sample.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+
+}
